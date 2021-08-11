6011818f303b1aff289fb97b8cbae5b073ae85ab
Johitan <johan52754@gmail.com>
2021-08-10 21:46:04 -0500

    Subiendo cambios en el enrutamiento

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/rewards-store" component={Home}/>
+        <Route exact path="/rewards-store/" component={Home}/>
         <Route exact path="/rewards-store/add-tokens" component={AddTokens}/>
         <Route exact path="/rewards-store/redeem-history" component={History}/>
       </Switch>
--- src/components/commons/Navbar/Navbar.jsx
+++ src/components/commons/Navbar/Navbar.jsx
@@ -6,9 +6,9 @@ export function Navbar(){
     return(
         <div className="navbar">
             <ul className={`navbar-ul`}>
-                <Link to="/" className="navbar-li"><li>HOME</li></Link>
-                <Link to="/add-tokens" className="navbar-li"><li>ADD TOKENS</li></Link>
-                <Link to="/redeem-history" className="navbar-li"><li>REDEEM HISTORY</li></Link>
+                <Link to="/rewards-store/" className="navbar-li"><li>HOME</li></Link>
+                <Link to="/rewards-store/add-tokens" className="navbar-li"><li>ADD TOKENS</li></Link>
+                <Link to="/rewards-store/redeem-history" className="navbar-li"><li>REDEEM HISTORY</li></Link>
             </ul>
         </div>
     )
